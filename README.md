f010cf4706807c7501269102f94d87aab4cf14d6
増田 博紀 <kuzrwkd@icloud.com>
2021-06-14 02:55:39 +0900

    add:scrapingに必要なパッケージを追加

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# SKY SCRAPER
+# SKYS SCRAPER
+
+## 開発環境構築
+
+.envファイルを作成し、書きをコピペする
+
+```
+PRISMA_DATABASE_URL=mysql://root:password@skys-scraper-db-svc:3306/skys-scraper
+DATABASE_HOST=skys-scraper-db-svc
+DATABASE_PORT=3306
+```
---
+++ ecosystem.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  apps: [
+    {
+      name: 'skys-api',
+      script: 'dist/main.js',
+      instances: 1,
+      autorestart: true,
+      watch: false,
+      max_memory_restart: '1G',
+      env_production: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
+}
