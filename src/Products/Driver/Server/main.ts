0b0c7644f2086f578445901c97b975f3215c13b7
増田 博紀 <kuzrwkd@icloud.com>
2021-07-31 07:15:34 +0900

    fix:リファクタリング

--- src/Products/Driver/Server/main.ts
+++ src/Products/Driver/Server/main.ts
@@ -9,18 +9,6 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from '@/Products/Driver/Server/app.module';
 
 /**
- * Tools import
- */
-import { container } from '@/Tools/Containers/Tools';
-
-/**
- * Tools entry
- */
-container.resolve<Tools.IDateTool>('DateTool');
-container.resolve<Tools.ILogTool>('Log');
-container.resolve<Tools.IRegExpVerExTool>('RegExpVerExTool');
-
-/**
  * service start
  */
 async function bootstrap() {
