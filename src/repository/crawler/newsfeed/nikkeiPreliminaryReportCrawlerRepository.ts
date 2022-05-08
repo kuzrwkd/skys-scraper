bc902066446936e39e87756ee8ce85d12ec8c29e
Hiroki Masuda <kuzrwkd@icloud.com>
2022-05-08 22:00:38 +0900

    :recycle: refactoring

--- src/repository/crawler/newsfeed/nikkeiPreliminaryReportCrawlerRepository.ts
+++ src/repository/crawler/newsfeed/nikkeiPreliminaryReportCrawlerRepository.ts
@@ -15,12 +15,13 @@ import logger, {
 export class NikkeiPreliminaryReportCrawlerRepository {
   async handle(u: string, organization: NewsFeed.Organization, tracking_id: string) {
     const { name: organizationName } = organization;
-    try {
-      const listCrawlerTrackingId = `${tracking_id}-lc-${createRandomString()}`;
-      const data: NewsFeed.NewsFeedCrawlerResult[] = [];
-      const browser = await puppeteer.launch(options);
-      const page = await browser.newPage();
+    const listCrawlerTrackingId = `${tracking_id}-lc-${createRandomString()}`;
+    const data: NewsFeed.NewsFeedCrawlerResult[] = [];
+    const browser = await puppeteer.launch(options);
+    const page = await browser.newPage();
+    const preliminaryReportUrl: string[] = [];
 
+    try {
       logger.info(`[${organizationName}] クローリング開始`, getStartCrawlingParams({ tracking_id }));
 
       const startTime = processStartTime();
@@ -34,7 +35,6 @@ export class NikkeiPreliminaryReportCrawlerRepository {
       );
 
       const endTime = processEndTime(startTime);
-      const preliminaryReportUrl: string[] = [];
 
       for (const link of preliminaryReportLinkList) {
         const url: string | undefined = await (await link.getProperty('href'))?.jsonValue();
@@ -51,8 +51,21 @@ export class NikkeiPreliminaryReportCrawlerRepository {
           result: preliminaryReportUrl,
         }),
       );
+    } catch (e) {
+      if (e instanceof Error) {
+        logger.error(
+          e.message,
+          getFailedParams({
+            tracking_id: tracking_id,
+            exception_class: e.name,
+            stacktrace: e.stack as string,
+          }),
+        );
+      }
+      return null;
+    }
 
-      // 各記事ページのタイトル投稿日時、更新日時を取得
+    try {
       const crawlingData = (
         await Promise.allSettled(
           preliminaryReportUrl.map(async (url: string) => {
@@ -63,7 +76,9 @@ export class NikkeiPreliminaryReportCrawlerRepository {
               getStartCrawlingParams({ tracking_id: individualCrawlerTrackingId }),
             );
             const startTime = processStartTime();
-            await page.goto(url);
+
+            await page.goto(url, { timeout: 0 });
+            await page.waitForSelector('div[class^="container_"] > main > article');
 
             const title = await page.$('h1[class^="title_"]');
             const createdAt = await page.$('[class^="TimeStamp_"] > time');
