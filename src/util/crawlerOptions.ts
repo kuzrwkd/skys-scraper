6ee176a1fc3abbb05cf1175f1f836502133a9223
Hiroki Masuda <kuzrwkd@icloud.com>
2023-01-04 06:21:18 +0900

    ⬆️ update puppeteer v19.4.1

--- src/util/crawlerOptions.ts
+++ src/util/crawlerOptions.ts
@@ -1,12 +1,5 @@
 export const options = {
-  args: [
-    '--no-sandbox',
-    '--disable-setuid-sandbox',
-    '--disable-dev-shm-usage',
-    '--single-process',
-    '--disable-web-security',
-    '--no-zygote',
-  ],
+  args: ['--no-sandbox', '--disable-setuid-sandbox'],
   headless: true,
   executablePath: process.env.PUPPETEER_EXECUTABLE_PATH,
 };
