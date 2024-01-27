3a89101e7a5583e8def27e15304f8d5e4854b0a6
Hiroki Masuda <kuzrwkd@icloud.com>
2024-01-27 19:53:39 +0900

    🔥 Remove cronJob dependency from StartNewsfeedCrawlerCommand
    Removed the CronJobParameters, CronJob from 'cron' dependencies and updated the handler function to directly call the newsfeedCrawlerInteract.handler() method in StartNewsfeedCrawlerCommand.ts file. This simplifies the handler's logic and reduces dependencies on third-party cron libraries.

--- app/src/commands/StartNewsfeedCrawlerCommand.ts
+++ app/src/commands/StartNewsfeedCrawlerCommand.ts
@@ -1,15 +1,8 @@
-import {CronJobParameters, CronJob} from 'cron';
 import * as yargs from 'yargs';
 import {INewsfeedCrawlerInteract} from '@/useCases';
 import {container} from '@/utils/yargsConfig';
 
 const newsfeedCrawlerInteract = container.resolve<INewsfeedCrawlerInteract>('NewsfeedCrawlerInteract');
-
-const jobOptions: CronJobParameters = {
-  cronTime: '*/60 * * * *',
-  onTick: () => newsfeedCrawlerInteract.handler(),
-  utcOffset: 0,
-};
 
 export default class StartNewsfeedCrawlerCommand implements yargs.CommandModule {
   command = 'start:newsfeed';
@@ -21,10 +14,7 @@ export default class StartNewsfeedCrawlerCommand implements yargs.CommandModule 
 
   async handler() {
     try {
-      const cronJob = new CronJob(jobOptions);
-      cronJob.start();
-      console.log('Start job time:', cronJob.nextDate().toISOTime());
-      return;
+      await newsfeedCrawlerInteract.handler();
     } catch (error) {
       console.error(error);
       process.exit(1);
