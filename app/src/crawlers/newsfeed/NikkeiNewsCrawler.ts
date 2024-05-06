56c3075ec747f8ce4a10d2aca5fab2a5fa23b91c
Hiroki Masuda <kuzrwkd@icloud.com>
2024-05-06 16:04:33 +0900

    Refactor Nikkei news crawler and interaction logic
    The crawler and interaction logic for Nikkei news are streamlined to improve efficiency. This includes simplified fetching of crawler indexes, enhanced logging for easier error handling, and better structuring of crawling sun-processes. Moreover, unnecessary overlapping processes and dependencies are removed.

--- app/src/crawlers/newsfeed/NikkeiNewsCrawler.ts
+++ app/src/crawlers/newsfeed/NikkeiNewsCrawler.ts
@@ -1,4 +1,4 @@
-import {convertISO8601ToUnix} from '@kuzrwkd/skys-core/date';
+import {convertISO8601ToEpochMillis, getEpochMillis, convertEpochMillisToISO8601} from '@kuzrwkd/skys-core/date';
 import logger, {
   startLogger,
   successLogger,
@@ -7,7 +7,6 @@ import logger, {
   processStartTime,
   processEndTime,
 } from '@kuzrwkd/skys-core/logger';
-import {createUuid} from '@kuzrwkd/skys-core/v4uuid';
 import playwright from 'playwright-core';
 import {ICrawler, CrawlerItem, CrawlerParams} from '@/crawlers';
 import {options} from '@/utils/crawlerOptions';
@@ -15,68 +14,49 @@ import {options} from '@/utils/crawlerOptions';
 export class NikkeiNewsCrawler implements ICrawler {
   async handle(params: CrawlerParams) {
     const {media, url: categoryUrl, category} = params;
-    const {name: mediaName, media_id: mediaId, domain} = media;
+    const {media_id: mediaId, domain} = media;
     const {category_id: categoryId} = category;
     const startTime = processStartTime();
-    logger.info(`[${mediaName}] Start crawling`, startLogger({categoryUrl}));
-    const browser = await playwright.chromium.launch(options);
-    const page = await browser.newPage();
-    await page.goto(categoryUrl, {timeout: 0});
-    await page.waitForSelector('main[class^="main_"]');
-    const articles = await page.locator('article[class^="sokuhoCard_"]').all();
-    const crawlingData = (
-      await Promise.allSettled(
-        articles.map(async item => {
-          try {
-            const linkElement = item.locator('div[class^="textArea_"] > a');
-            const timeElement = item.locator('div[class^="dateContainer_"] time');
-            const title = await linkElement.innerText();
-            const articlePath = (await linkElement.getAttribute('href')) ?? undefined;
-            const url = articlePath ? `${domain}${articlePath}` : undefined;
-            const lastUpdateDate = (await timeElement.getAttribute('datetime')) ?? undefined;
-            if (!lastUpdateDate) {
-              throw new Error('lastUpdateDate is undefined');
-            }
-            if (!url) {
-              throw new Error('url is undefined');
-            }
-            const result: CrawlerItem = {
-              id: createUuid(),
-              title,
-              url,
-              media_id: mediaId,
-              category_id: categoryId,
-              last_update_date: convertISO8601ToUnix(lastUpdateDate),
-            };
-            logger.info(`[${mediaName}] Process crawling`, processLogger({result}));
-            return result;
-          } catch (error) {
-            if (error instanceof Error) {
-              logger.error(
-                error.message,
-                failedLogger({
-                  exception_class: error.name,
-                  stacktrace: error.stack,
-                }),
-              );
-            }
-          }
-        }),
-      )
-    ).map(e => {
-      if (e.status === 'fulfilled' && e.value) {
-        return e.value;
-      }
-    });
-    await browser.close();
-    const endTime = processEndTime(startTime);
-    logger.info(`[${mediaName}] End crawling`, successLogger({time: endTime}));
-    const newsfeedCrawlerResults: CrawlerItem[] = [];
-    for (const item of crawlingData) {
-      if (item) {
-        newsfeedCrawlerResults.push(item);
+    logger.info(`Start nikkei ${category.name} category crawling.`, startLogger());
+
+    try {
+      const browser = await playwright.chromium.launch(options);
+      const page = await browser.newPage();
+      await page.goto(categoryUrl);
+      const newsItems = await page.locator('article[class^="sokuhoCard_"]').all();
+      const makeNewsLinks = newsItems.map(async item => {
+        const linkElement = item.locator('div[class^="textArea_"] > a');
+        const timeElement = item.locator('div[class^="dateContainer_"] time');
+        const lastUpdateDate =
+          (await timeElement.getAttribute('datetime')) ?? convertEpochMillisToISO8601(getEpochMillis());
+        const title = await linkElement.innerText();
+        const relativeUrl = (await linkElement.getAttribute('href')) ?? undefined;
+        const absoluteUrl = relativeUrl ? `${domain}${relativeUrl}` : undefined;
+        if (!absoluteUrl) {
+          throw new Error('Invalid absoluteUrl.');
+        }
+        const result = {
+          title,
+          url: absoluteUrl,
+          media_id: mediaId,
+          category_id: categoryId,
+          last_publish_date: convertISO8601ToEpochMillis(lastUpdateDate),
+        } as CrawlerItem;
+        logger.info(`Processing nikkei ${category.name} category crawling.`, processLogger());
+        return result;
+      });
+      const newsLinks = await Promise.all(makeNewsLinks);
+      await browser.close();
+      const endTime = processEndTime(startTime);
+      logger.info(
+        `Successfully nikkei ${category.name} category crawling.`,
+        successLogger({result: newsLinks, time: endTime}),
+      );
+      return newsLinks;
+    } catch (error) {
+      if (error instanceof Error) {
+        logger.error(`Failed to nikkei ${category.name} category crawling.`, failedLogger({result: error}));
       }
     }
-    return newsfeedCrawlerResults;
   }
 }
--- app/src/useCases/interact/NikkeiNewsCrawlerInteract.ts
+++ app/src/useCases/interact/NikkeiNewsCrawlerInteract.ts
@@ -1,8 +1,9 @@
-import {categoryTable, mediaTable, newsfeedIndexTable, newsfeedTable} from '@kuzrwkd/skys-core/dynamodb';
-import {MediaId, MediaSchema, CategorySchema} from '@kuzrwkd/skys-core/entities';
-import {failedLogger} from '@kuzrwkd/skys-core/logger';
-import {injectable, inject} from 'tsyringe';
-import {ICrawler, CrawlerItem} from '@/crawlers';
+import * as process from 'node:process';
+import {master, newsfeedTable} from '@kuzrwkd/skys-core/dynamodb';
+import {CategorySchema, MediaId, MediaSchema} from '@kuzrwkd/skys-core/entities';
+import logger, {failedLogger, startLogger} from '@kuzrwkd/skys-core/logger';
+import {inject, injectable} from 'tsyringe';
+import {ICrawler} from '@/crawlers';
 
 type NikkeiCrawlerIndex = {
   id: string;
@@ -22,84 +23,70 @@ export class NikkeiNewsCrawlerInteract implements INikkeiNewsCrawlerInteract {
 
   async getCrawlerIndex() {
     try {
-      const masterData = await Promise.all([
-        newsfeedIndexTable.getAllItems(),
-        mediaTable.getAllItems(),
-        categoryTable.getAllItems(),
-      ])
-        .then(result => {
-          const [newsfeedIndexAllItems, mediaAllItems, categoryAllItems] = result;
-          return {
-            newsfeedIndexAllItems: newsfeedIndexAllItems ?? [],
-            mediaAllItems: mediaAllItems ?? [],
-            categoryAllItems: categoryAllItems ?? [],
-          };
-        })
-        .catch(error => {
-          throw new Error(error.message);
-        });
-      return masterData.newsfeedIndexAllItems.reduce((acc, item) => {
-        if (item.media_id === MediaId.NIKKEI) {
-          return [
-            ...acc,
-            {
-              id: item.id,
-              media: masterData.mediaAllItems.find(_ => _.media_id === MediaId.NIKKEI) as MediaSchema,
-              category: masterData.categoryAllItems.find(_ => _.category_id === item.category_id) as CategorySchema,
-              url: item.url,
-            },
-          ];
-        }
-        return acc;
-      }, [] as NikkeiCrawlerIndex[]);
-    } catch (error) {}
+      const masterData = await master.get();
+      if (masterData) {
+        return masterData.newsfeedIndexAllItems.reduce((acc, item) => {
+          if (item.media_id === MediaId.NIKKEI) {
+            return [
+              ...acc,
+              {
+                id: item.id,
+                media: masterData.mediaAllItems.find(_ => _.media_id === MediaId.NIKKEI) as MediaSchema,
+                category: masterData.categoryAllItems.find(_ => _.category_id === item.category_id) as CategorySchema,
+                url: item.url,
+              },
+            ];
+          }
+          return acc;
+        }, [] as NikkeiCrawlerIndex[]);
+      }
+    } catch (error) {
+      if (error instanceof Error) {
+        logger.error('Failed to getCrawlerIndex.', failedLogger({result: error}));
+      }
+    }
   }
 
   async handler() {
     try {
+      logger.info('Start nikkei news crawling.', startLogger());
       const nikkeiCrawlerIndex = await this.getCrawlerIndex();
       if (!nikkeiCrawlerIndex) {
-        throw new Error('');
+        throw new Error('Invalid nikkeiCrawlerIndex.');
       }
-      const crawlerResult: CrawlerItem[] = [];
-      await Promise.allSettled(
-        nikkeiCrawlerIndex.map(async item => {
-          const {media, url, category} = item;
-          const crawlerParams = {url, media, category};
-          const result = await this.nikkeiNewsCrawler.handle(crawlerParams);
-          if (result) {
-            crawlerResult.push(...result);
-          }
-        }),
-      );
-      for (const crawlerItem of crawlerResult) {
-        if (!crawlerItem.url) {
-          throw new Error('crawler url not found');
+      const crawlers = nikkeiCrawlerIndex.map(async item => {
+        const {media, url, category} = item;
+        const crawlerParams = {url, media, category};
+        return this.nikkeiNewsCrawler.handle(crawlerParams);
+      });
+
+      const result = await Promise.all(crawlers);
+
+      for (const item of result.flat()) {
+        if (!item) {
+          throw new Error('Invalid crawling result item.');
         }
-        const newsfeedItem = await newsfeedTable.getItemByUrl(crawlerItem.url);
+        const newsfeedItem = await newsfeedTable.getItemByUrl(item.url);
         if (!newsfeedItem) {
-          await newsfeedTable.putItem(crawlerItem);
+          await newsfeedTable.putItem(item);
           continue;
         }
-        if (crawlerItem.last_update_date && crawlerItem.last_update_date !== newsfeedItem.last_update_date) {
-          await newsfeedTable.updateLastPostDate({
+        if (item.last_publish_date && item.last_publish_date !== newsfeedItem.last_publish_date) {
+          await newsfeedTable.updateLastPublishDate({
             ...newsfeedItem,
-            last_update_date: crawlerItem.last_update_date,
+            last_publish_date: item.last_publish_date,
           });
         }
-        if (!newsfeedItem.category_ids.includes(crawlerItem.category_id)) {
+        if (!newsfeedItem.category_ids.includes(item.category_id)) {
           await newsfeedTable.updateCategoryIds({
             ...newsfeedItem,
-            category_id: crawlerItem.category_id,
+            category_id: item.category_id,
           });
         }
       }
     } catch (error) {
       if (error instanceof Error) {
-        failedLogger({
-          exception_class: error.name,
-          stacktrace: error.stack,
-        });
+        logger.error('Failed to nikkei news crawling.', failedLogger({result: error}));
       }
     }
   }
